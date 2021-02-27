5be920b6abdf01460d4161485daa6b6e55958fc0
pikoUsername <galymzhan.amantaj@gmail.com>
2021-02-27 18:53:01 +0300

    help handler state update

--- app/handlers/users/help.py
+++ app/handlers/users/help.py
@@ -6,7 +6,7 @@ from app.loader import dp, i18n
 _ = i18n.gettext
 
 
-@dp.message_handler(CommandHelp())
+@dp.message_handler(CommandHelp(), state="*")
 async def bot_help(message: types.Message):
     text = _('Список команд: \n'
              '/start - Начать диалог\n'
