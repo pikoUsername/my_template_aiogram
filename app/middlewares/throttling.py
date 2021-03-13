77c9db0c858bcd6c502d97aca02eed370387158d
pikoUsername <galymzhan.amantaj@gmail.com>
2021-03-13 13:18:16 +0300

    Removed unused local variable

--- app/middlewares/throttling.py
+++ app/middlewares/throttling.py
@@ -18,7 +18,7 @@ class ThrottlingMiddleware(BaseMiddleware):
         self.prefix = key_prefix
         super(ThrottlingMiddleware, self).__init__()
 
-    async def on_process_message(self, message: types.Message, data: dict):
+    async def on_process_message(self, message: types.Message, _):
         handler = current_handler.get()
         dispatcher = Dispatcher.get_current()
         if handler:
